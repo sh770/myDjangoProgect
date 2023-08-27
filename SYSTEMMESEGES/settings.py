d0fd74b89be5da87f6d72c6a2f4d53df9ebf132c
Shalom Harpaz <sh770h@gmail.com>
2023-08-28 00:43:18 +0300

    28

--- SYSTEMMESEGES/settings.py
+++ SYSTEMMESEGES/settings.py
@@ -129,6 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
+    "https://sh770.github.io",
     "http://localhost:3000", 
 ]
