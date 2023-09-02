f90d93f84829264111a830127d29ddeb3db1858a
Shalom Harpaz <sh770h@gmail.com>
2023-09-03 01:04:15 +0300

    03/09

--- API/views.py
+++ API/views.py
@@ -1,13 +1,55 @@
 from django.shortcuts import render
 # from django.http import JsonResponse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
 from .models import Message
 from .serializers import MessageSerializer
 
 
 # Create your views here.
 
+#Login/SignIn
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        token['email'] = user.email
+        # ...
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+
+# register/signup
+@api_view(['GET','POST'])
+def signUp(request):
+    user=User.objects.create_user(
+        username= request.data["username"],
+        email=request.data["email"],
+        password=request.data["password"]
+    )
+    token = RefreshToken.for_user(user)
+    return Response({
+        'access': str(token.access_token),
+        'refresh': str(token),
+        'username': user.username,
+        'email': user.email
+    })
+
+
+# first step
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
@@ -30,12 +72,22 @@ def getRoutes(request):
             'description': 'Deletes and exiting message'
         },
     ]
-    # return JsonResponse(routes , safe=False)
-    return Response(routes)
+    #return JsonResponse(routes , safe=False)
+    return Response (routes)
 
-#all meseges
+
+# get all messages
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getReceivedMessages(request):
     messages = Message.objects.all()
-    serializer = MessageSerializer(messages, many = True)
-    return Response(serializer.data) 
+    serializer = MessageSerializer(messages, many=True)
+    return Response (serializer.data) 
+
+# get a message
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getReceivedMessage(request, id):
+    message = Message.objects.get(id=id)
+    serializer = MessageSerializer(message, many=False)
+    return Response (serializer.data) 
