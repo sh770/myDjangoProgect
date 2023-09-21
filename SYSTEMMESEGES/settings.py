f9762ac1f85292486de2893c72bae5d2291f70e9
Shalom Harpaz <sh770h@gmail.com>
2023-09-21 23:44:02 +0300

    21/09

--- SYSTEMMESEGES/settings.py
+++ SYSTEMMESEGES/settings.py
@@ -11,15 +11,21 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+import os
 
+# import psycopg as Database
+
+# from psycopg2 import psycopg as Database
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# from dj_database_url import dj-database-url
+from dj_database_url import config as db_config
+
+# DATABASES = {'default': db_config(default=os.environ.get('DATABASE_URL'))}
+
+# DATABASES = {'default': db_config(default=os.environ.get('postgres://sh770_user:5K0fTTQ77js5r92jr0y2LeVLl8UPoFAK@dpg-ck4u5m29hnms739pd5r0-a.oregon-postgres.render.com/sh770'))}
 
 from dotenv import load_dotenv
-import os
-
 load_dotenv()
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -32,14 +38,17 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-vumrfnkxvxp!3v!3$&^6fu1og0_m$4q8eu1h$9r@kz$=+lz==t'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+# DEBUG = False
+
+DEBUG = os.environ.get('DEBUG')
 
 
 # Raises Django's ImproperlyConfigured
 # exception if SECRET_KEY not in os.environ
 
 ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS = ['*']
 
 # Application definition
 
