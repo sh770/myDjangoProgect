9af49d0faea18cfca02695d25a8f2bb191c91892
Shalom Harpaz <sh770h@gmail.com>
2023-09-24 01:02:51 +0300

    3

--- SYSTEMMESEGES/settings.py
+++ SYSTEMMESEGES/settings.py
@@ -19,7 +19,9 @@ import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 env = environ.Env()
 
@@ -88,7 +90,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
     ]
 
 REST_FRAMEWORK = {
@@ -210,9 +212,7 @@ USE_TZ = True
 # STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
