240c67c4fb65d9905b59cdddb669278a4feb768c
Shalom Harpaz <sh770h@gmail.com>
2023-09-24 00:08:13 +0300

    s

--- SYSTEMMESEGES/settings.py
+++ SYSTEMMESEGES/settings.py
@@ -206,7 +206,7 @@ STATIC_ROOT = BASE_DIR / 'staic'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
