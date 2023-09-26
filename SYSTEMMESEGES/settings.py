6972d307aecefe99945ee4cf6b73e7722436826f
Shalom Harpaz <sh770h@gmail.com>
2023-09-26 18:28:10 +0300

    26

--- SYSTEMMESEGES/settings.py
+++ SYSTEMMESEGES/settings.py
@@ -202,12 +202,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
