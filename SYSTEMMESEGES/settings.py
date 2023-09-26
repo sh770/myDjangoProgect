6769037d40590f077f59ddc08286b5e57a2c8650
Shalom Harpaz <sh770h@gmail.com>
2023-09-26 17:56:53 +0300

    26

--- SYSTEMMESEGES/settings.py
+++ SYSTEMMESEGES/settings.py
@@ -202,7 +202,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_URL = '/static/'
+if not DEBUG:
+    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # MEDIA_URL = '/media/'
 
 # STATIC_ROOT = BASE_DIR / 'staic'
@@ -211,7 +218,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 
